8039a82353bb4f52ea9560e81e757c6efcf157af
Egor Bugay <egbugaj@gmail.com>
2024-10-21 21:56:17 +0600

    feature: add navigation

---
+++ app/src/main/java/com/example/marvel_application/presentation/navigation/NavGraph.kt
@@ -0,0 +1,58 @@
+package com.example.marvel_application.presentation.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.marvel_application.presentation.ui.screens.character_card_screen.CharacterCardScreen
+import com.example.marvel_application.presentation.ui.screens.main_screen.MainScreen
+
+private object Screens {
+    object Main {
+        fun route() = "main_screen"
+    }
+
+    object Character {
+        fun characterId(): String = "characterId"
+        fun baseRoute() = "character_card_screen/{characterId}"
+        fun route(id: Int) = "character_card_screen/$id"
+    }
+}
+
+@Composable
+fun NavGraph(
+    modifier: Modifier = Modifier,
+    navController: NavHostController = rememberNavController()
+) {
+    NavHost(
+        modifier = modifier,
+        navController = navController,
+        startDestination = Screens.Main.route(),
+    ) {
+        composable(route = Screens.Main.route()) {
+            MainScreen(
+                onNavigateToCharacterCardScreen = {
+                    navController.navigate(Screens.Character.route(it))
+                }
+            )
+        }
+        composable(
+            route = Screens.Character.baseRoute(),
+            arguments = listOf(navArgument(Screens.Character.characterId()) {
+                type = NavType.IntType
+            })
+        ) { backStackEntry ->
+            val characterId = backStackEntry.arguments?.getInt(Screens.Character.characterId())
+            characterId?.let {
+                CharacterCardScreen(
+                    characterId = characterId,
+                    onClick = { navController.popBackStack() }
+                )
+            }
+        }
+    }
+}
