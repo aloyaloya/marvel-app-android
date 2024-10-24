62f52ec00855195671e017e320dd927b33e59586
Egor Bugay <egbugaj@gmail.com>
2024-10-24 20:56:51 +0600

    feature: add scrolling animation

--- app/src/main/java/com/example/marvel_application/presentation/ui/screens/main_screen/components/CharacterList.kt
+++ app/src/main/java/com/example/marvel_application/presentation/ui/screens/main_screen/components/CharacterList.kt
@@ -1,19 +1,20 @@
 package com.example.marvel_application.presentation.ui.screens.main_screen.components
 
 import androidx.compose.foundation.gestures.snapping.rememberSnapFlingBehavior
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.unit.dp
 import com.example.marvel_application.R
 import com.example.marvel_application.presentation.ui.screens.main_screen.CharacterViewModel
+import kotlin.math.absoluteValue
 
 @Composable
 fun CharactersList(
@@ -30,16 +31,29 @@ fun CharactersList(
         modifier = modifier,
         state = lazyListState,
         flingBehavior = snapBehavior,
-        horizontalArrangement = Arrangement.spacedBy(12.dp),
         contentPadding = PaddingValues(dimensionResource(id = R.dimen.large_padding))
     ) {
-        items(characters) { character ->
+        itemsIndexed(characters) { index, character ->
+            val itemOffset = lazyListState.calculateCurrentOffsetForItem(index)
+            val scale = 1f - ((itemOffset.absoluteValue / 3000f).coerceIn(0f, 0.1f))
+
             CharacterCard(
-                modifier = Modifier.fillParentMaxSize(),
+                modifier = Modifier
+                    .fillParentMaxWidth()
+                    .graphicsLayer {
+                        scaleX = scale
+                        scaleY = scale
+                    },
                 id = character.id,
                 viewModel = viewModel,
                 onClick = onClick
             )
         }
     }
+}
+
+@Composable
+fun LazyListState.calculateCurrentOffsetForItem(index: Int): Float {
+    val layoutInfo = layoutInfo.visibleItemsInfo.firstOrNull { it.index == index }
+    return layoutInfo?.offset?.toFloat() ?: 0f
 }
