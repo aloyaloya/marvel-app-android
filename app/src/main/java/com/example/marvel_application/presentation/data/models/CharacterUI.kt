c092e7fbc052ed6f9d850117a9fa2b0c70a46d72
Egor Bugay <egbugaj@gmail.com>
2024-11-20 01:44:05 +0600

    add mappers

---
+++ app/src/main/java/com/example/marvel_application/presentation/data/models/CharacterUI.kt
@@ -0,0 +1,8 @@
+package com.example.marvel_application.presentation.data.models
+
+data class CharacterUI(
+    val id: Int,
+    val name: String,
+    val description: String,
+    val thumbnailUrl: String,
+)
--- app/src/main/java/com/example/marvel_application/presentation/data/repository/CharactersMapper.kt
+++ app/src/main/java/com/example/marvel_application/presentation/data/repository/CharactersMapper.kt
@@ -1,12 +1,53 @@
 package com.example.marvel_application.presentation.data.repository
 
-import com.example.marvel_application.presentation.data.model.MarvelCharacter
+import com.example.marvel_application.presentation.data.database.entity.CharacterEntity
+import com.example.marvel_application.presentation.data.models.CharacterUI
+import com.example.marvel_application.presentation.data.models.MarvelCharacterDTO
+import javax.inject.Inject
 
-fun List<MarvelCharacter>.mapValidCharacters(): List<MarvelCharacter> {
-    return this.filter { character ->
-        character.description.isNotEmpty() &&
-                character.thumbnail?.path != null &&
-                character.thumbnail.extension == "jpg" &&
-                !character.thumbnail.path.contains("image_not_available", ignoreCase = true)
+class CharacterMapper @Inject constructor() {
+
+    fun mapDtoToUI(dto: MarvelCharacterDTO): CharacterUI {
+        val url = (dto.thumbnail!!.path + "." + dto.thumbnail.extension).toHttpsPrefix()
+
+        return CharacterUI(
+            id = dto.id,
+            name = dto.name,
+            description = dto.description,
+            thumbnailUrl = url
+        )
     }
+
+    fun mapEntityToUI(entity: CharacterEntity): CharacterUI {
+        val url = (entity.path + "." + entity.extension).toHttpsPrefix()
+
+        return CharacterUI(
+            id = entity.id,
+            name = entity.name,
+            description = entity.description,
+            thumbnailUrl = url
+        )
+    }
+
+    fun mapDtoToEntity(dto: MarvelCharacterDTO): CharacterEntity {
+        return CharacterEntity(
+            id = dto.id,
+            name = dto.name,
+            description = dto.description,
+            path = dto.thumbnail!!.path,
+            extension = dto.thumbnail.extension
+        )
+    }
+
+    fun mapValidCharacters(characters: List<MarvelCharacterDTO>): List<MarvelCharacterDTO> {
+        return characters.filter { character ->
+            character.description.isNotEmpty() &&
+                    character.thumbnail?.path != null &&
+                    character.thumbnail.extension == "jpg" &&
+                    !character.thumbnail.path.contains("image_not_available", ignoreCase = true)
+        }
+    }
+
+    private fun String.toHttpsPrefix(): String =
+        if (startsWith("http://")) replace("http://", "https://") else this
 }
